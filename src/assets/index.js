a1e2865cb09a2e9ddcbdbd234f9093747401b9af
haris kamran <harisk543@hotmail.com>
2025-08-11 14:29:10 +0100

    C captial clucker

--- src/assets/index.js
+++ src/assets/index.js
@@ -32,7 +32,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 import Crypto from "./Crypto.png";
 import Terranova from "./Terranova.png";
-import clucker from "./clucker.png";
+import clucker from "./Clucker.png";
 
 export {
   logo,
